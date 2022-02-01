9bccf01257417489c88cf4e74d6205ae0ccfd9b4
JuanMelinir <juan.melinir@est.fi.uncom.edu.ar>
2022-01-31 21:53:40 -0300

    modifique class por className

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,27 +5,27 @@ import ShoppingCart from './CartWidget';
 const Navbar=()=>{
     return (
       <>
-<nav class="navbar navbar-expand-lg navbar-light  bg-danger">
-  <a class="navbar-brand" href="#"><img src={logoMarca} width="150" height="40"></img></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-light  bg-danger">
+  <a className="navbar-brand" href="#"><img src={logoMarca} width="150" height="40"></img></a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#" id="color-item">Categoría 1</a>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="#" id="color-item">Categoría 1</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#" id="color-item">Categoría 2</a>
+      <li className="nav-item">
+        <a className="nav-link" href="#" id="color-item">Categoría 2</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#" id="color-item">Categoría 3</a>
+      <li className="nav-item">
+        <a className="nav-link" href="#" id="color-item">Categoría 3</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#" id="color-item">Categoría 4</a>
+      <li className="nav-item">
+        <a className="nav-link" href="#" id="color-item">Categoría 4</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#" id="color-item"><ShoppingCart/></a>
+      <li className="nav-item">
+        <a className="nav-link" href="#" id="color-item"><ShoppingCart/></a>
       </li>
     </ul>
   </div>
