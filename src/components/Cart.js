a3d837809638488b97bc81a12ccd92fa1a6c250f
JuanMelinir <juan.melinir@est.fi.uncom.edu.ar>
2022-03-08 16:36:31 -0300

    desafio item collection II

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,47 +2,109 @@
 import { useContext, useEffect } from 'react';
 import { CartContext } from './CartContext';
 import CartContextProvider from './CartContext';
+import { doc, setDoc, collection, serverTimestamp, updateDoc, increment } from 'firebase/firestore';
+import db from '../utils/firebaseConfig';
 
 const Cart = () => {
-    const {removeItem,totalCost}=useContext(CartContext);
+    const { removeItem, totalCost } = useContext(CartContext);
     const carrito = useContext(CartContext);
- console.log(carrito.cartList);
+    const crearOrden = () => {
+        let orden = {
+            buyer: {
+                email: "leo_messi@gmail.com",
+                name: "Leo Messi",
+                phone: "123456"
+            },
+            date: serverTimestamp(),
+            items: carrito.cartList.map((item) => {
+                return { id: item.id, price: item.price, title: item.title, cantidad: item.cantidadItems }
+            }),
+            total: carrito.totalCost
+        }
+        const crearOrdenEnFirestore = async () => {
+            const newOrderRef = doc(collection(db, "orders"))
+            await setDoc(newOrderRef, orden)
+            return newOrderRef;
+        }
+        crearOrdenEnFirestore()
+            .then(result => {
+                alert("tu orden ha sido creada: " + result.id); carrito.clear();
+                carrito.cartList.map(async (item) => {
+                    const itemRef = doc(db, "remeras", item.id);
+                    await updateDoc(itemRef, { stock: increment(-item.cantidadItems) })
+                })
+            })
+            .catch(error => console.log(error));
+    }
+
+
+
     return (
-        <div className="contenedorItems">
-       <h3>Carrito</h3>
-       {
-        carrito.cartList.length > 0
-       ?<button className="btn btn-dark" type="button" id="btnVaciar" onClick={
-           carrito.clear}>Vaciar</button>
-       :<h5>Su carro está vacio</h5>  }
-            
+        <div className="contenedorItemsCarrito">
+            <h3>Carrito</h3>
+            {
+                carrito.cartList.length > 0
+                    ? <button className="btn btn-dark" type="button" id="btnVaciar" onClick={
+                        carrito.clear}>Vaciar</button>
+                    :<div className="contenedorItemDescripcion"><h5>Su carro está vacio</h5></div>}
+
+                    <div class="row">
+                   <div class="col-8">{carrito.cartList.length > 0 && carrito.cartList.map(item => (
                 
-                {carrito.cartList.length>0 && carrito.cartList.map(item=>(
                 <div className="contenedorItemDescripcion">
-                
-                <div className="contenedorItemDetallesCarrito">
-                <img src={item.pictureUrl}></img>
-                <div className="contenedorItem">
-                <h5>{item.title}</h5>    
-                <button className="btn btn-dark" type="button" onClick={()=>removeItem(item.id)}>Eliminar</button>
+
+                    <div className="contenedorItemDetallesCarrito">
+                        <div className="contenedorImagen">
+                            <img src={item.pictureUrl}></img>
+                        </div>
+
+                        <div className="contenedorTituloItemCarrito">
+                            <h5>{item.title}</h5>
+                            <button className="btn btn-dark " type="button" id="btnELiminar" onClick={() => removeItem(item.id)}>Eliminar</button>
+                        </div>
+                        <div className="contenedorItem">
+                            <h6 > cantidad: {item.cantidadItems} items</h6>
+                            <h6 >precio por unidad: $ {item.price}</h6>
+                            <h6 >precio total: $ {item.price * item.cantidadItems}</h6>
+                        </div>
+                    </div>
+
+
+
                 </div>
-                </div>
-                <div className="contenedorItem">
-                <h6 className="textDescripcion">{item.cantidadItems} items</h6>
-                <h6 className="textPrecio">precio por unidad: $ {item.price}</h6>
-                <h6 className="textPrecio">precio total: $ {item.price*item.cantidadItems}</h6>
-                </div>
-                </div>    
-                
-            
+
+
             )
             )
+
+            }</div>
+                   <div class="col-4"><div className="contenedorTabla">
+                    
+                    <table class="table">
+
+                        <thead>
+                        <h6>Pedido N° xxxxxxx</h6>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Subtotal</th>
+                                <td>{totalCost.reduce((a, b) => a + b, 0)}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Descuento</th>
+                                <td>-xxxxx</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Total</th>
+                                <td>{totalCost.reduce((a, b) => a + b, 0)}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button className="btn btn-dark " type="button" id="btnELiminar" onClick={crearOrden}>Confirmar pedido</button>
+                </div></div>
+                   </div>
             
-            }   
-            <div> 
-                <h6>Total: {totalCost.reduce((a,b)=>a+b,0)}</h6>
-                </div>
-            </div>
+        </div>
     );
 }
 
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,7 +6,7 @@ return(
     {
     items.length > 0
     ? items.map(item=><Item key={item.id} id={item.id} title={item.title} pictureUrl={item.pictureUrl} price={item.price} stock={item.stock}/>)
-    :<p>cargando...</p>
+    :<p><span class="spinner-grow text-danger"></span></p>
     }
     </>
 );
